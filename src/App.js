62e1da288c06eec32253c5f982ceb1e0d8852abc
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2021-07-12 18:11:30 -0300

    Correção de pontos falhos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
-import { TextBase,View ,StyleSheet} from 'react-native';
-import Simples from './componentes/Simples'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
 
-
-export default class App extends Component{
-render(){
-return (
-<View style={estilos.container}>
-<Simples/>
-</View>
-
-
-);
-}
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Ola Mundo</Text>
+            </View>
+        );
+    }
 
 }
